2d3ae6314e6329e9aa58e2045e32562d769d4905
fordefam12 <136336831+fordefam12@users.noreply.github.com>
2023-07-24 13:27:09 -0500

    port correction

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const express = require("express");
 const path = require("path");
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const { v4: uuidv4 } = require("uuid");
 
 app.use(express.static("public"));
